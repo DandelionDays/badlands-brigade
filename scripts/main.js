c0344bc06776d76d4ab29b1efff4a25326d2ae2e
Dandelion <dandelionxwitch@tutanota.com>
2024-11-10 19:52:14 -0700

    AHAHAHAHAHA I AM SO POWERFUL I FINISHED THE HAMBURGER MENU

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+const buttonOpen = document.getElementById('buttonOpen');
+const buttonClose = document.getElementById('buttonClose');
+
+function openMobileMenu() {
+  buttonOpen.setAttribute('aria-expanded', 'true');
+}
+
+function closeMobileMenu() {
+  buttonOpen.setAttribute('aria-expanded', 'false');
+}
+
+buttonOpen.addEventListener('click', openMobileMenu(),
+  console.log("Open Menu")
+);
+
+buttonClose.addEventListener('click', closeMobileMenu(),
+  console.log("Close Menu"));
